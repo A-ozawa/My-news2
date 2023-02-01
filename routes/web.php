3379a6fcf2da3b66e2381f789b32b7f39ba649ac
a-ozawa <pataya0527@gmail.com>
2023-02-01 06:01:20 +0000

    Laravel8の課題

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,17 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
-});//ログインしていない場合のリダイレクト処理
-
-use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit', 'edit')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function (){
+    Route::get('news/create','add')->name('news.add');
+    Route::post('news/create','create')->name('news.create');
 });
 
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function (){
+    Route::get('profile/create','add')->name('profile.add');
+    Route::post('profile/create','create')->name('profile.create');
+    Route::post('profile/create','edit')->name('profile.update');
+});
 
 Auth::routes();
 
